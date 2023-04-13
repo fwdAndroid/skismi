3aa77854a62025569d41306ed55e1ec0432dda76
fwdAndroid <fwdkaleem@gmail.com>
2023-04-13 12:23:58 +0500

    Logout Feature Implemented

--- lib/main_screen_pages/settings.dart
+++ lib/main_screen_pages/settings.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:skismi/auth/login_screen.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -22,19 +24,6 @@ class _SettingsState extends State<Settings> {
           Image.asset(
             "assets/logo.png",
             height: 250,
-          ),
-          Divider(
-            color: Colors.white,
-          ),
-          ListTile(
-            title: Text(
-              "Change Language",
-              style: TextStyle(color: Colors.white),
-            ),
-            trailing: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.white,
-            ),
           ),
           Divider(
             color: Colors.white,
@@ -92,6 +81,57 @@ class _SettingsState extends State<Settings> {
             color: Colors.white,
           ),
           ListTile(
+            onTap: () {
+              showModalBottomSheet<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: 200,
+                    color: Colors.black,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Text(
+                            'Are you sure you want to exist?',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                child: const Text('Yes'),
+                                onPressed: () async {
+                                  await FirebaseAuth.instance
+                                      .signOut()
+                                      .then((value) => {
+                                            Navigator.pushReplacement(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (builder) =>
+                                                        LoginScreen()))
+                                          });
+                                },
+                              ),
+                              TextButton(
+                                child: const Text('No'),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
             title: Text(
               "Logout",
               style: TextStyle(color: Colors.white),
