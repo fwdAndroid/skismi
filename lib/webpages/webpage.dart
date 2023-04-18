12a9e8db301b690802652ffb1ef5a324380c8ceb
fwdAndroid <fwdkaleem@gmail.com>
2023-04-19 00:20:37 +0500

    Web Page Task Is done

--- lib/webpages/webpage.dart
+++ lib/webpages/webpage.dart
@@ -17,32 +17,55 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(widget.title),
         backgroundColor: Colors.black,
-        actions: [
-          TextButton(
-              onPressed: () {
-                _controller.reload();
-              },
-              child: Text("Reload"))
-        ],
+        // actions: [
+        //   TextButton(
+        //       onPressed: () {
+        //         _controller.reload();
+        //       },
+        //       child: Text("Reload"))
+        // ],
       ),
-      body: WebViewWidget(
-        controller: _controller
-          ..setJavaScriptMode(JavaScriptMode.unrestricted)
-          ..setBackgroundColor(const Color(0x00000000))
-          ..setNavigationDelegate(
-            NavigationDelegate(
-              onProgress: (int progress) {
-                // Update loading bar.
-              },
-              onPageStarted: (String url) {},
-              onPageFinished: (String url) {},
-              onWebResourceError: (WebResourceError error) {},
+      body: Column(children: [
+        Expanded(
+          child: WebViewWidget(
+            controller: _controller
+              ..setJavaScriptMode(JavaScriptMode.unrestricted)
+              ..setBackgroundColor(const Color(0x00000000))
+              ..setNavigationDelegate(
+                NavigationDelegate(
+                  onProgress: (int progress) {
+                    // Update loading bar.
+                  },
+                  onPageStarted: (String url) {},
+                  onPageFinished: (String url) {},
+                  onWebResourceError: (WebResourceError error) {},
+                ),
+              )
+              ..loadRequest(Uri.parse(widget.url)),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Go Back")),
+            SizedBox(
+              width: 20,
             ),
-          )
-          ..loadRequest(Uri.parse(widget.url)),
-      ),
+            TextButton(
+                onPressed: () {
+                  _controller.reload();
+                },
+                child: Text("Reload"))
+          ],
+        )
+      ]),
     );
   }
 }
